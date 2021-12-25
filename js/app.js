a65435ef93ecba7cf3adccf3f081547207b4afa3
Phùng An Bình <binhphung18032000>
2021-12-26 00:26:08 +0700

    updated1226

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
+// $(document).ready(function(){
+//     $('.eye').click(function(){
+//         $(this).toggleClass('open');
+//         $(this).children('i').toggleClass('fa-eye-slash fa-eye');
+//         if($(this).hasClass('open')){
+//             $(this).prev('.see_pass').attr('type', 'text');
+//         }else{
+//             $(this).prev('.see_pass').attr('type', 'password');
+//         }
+//     });
+// });
+
+//Thêm class .active khi click vào 1 mục nào đó trên main-menu
 $(document).ready(function(){
-    $('.eye').click(function(){
-        $(this).toggleClass('open');
-        $(this).children('i').toggleClass('fa-eye-slash fa-eye');
-        if($(this).hasClass('open')){
-            $(this).prev('.see_pass').attr('type', 'text');
-        }else{
-            $(this).prev('.see_pass').attr('type', 'password');
-        }
+    $('#main_menu>li.bg-active').click(function(){
+        $('#main_menu>li.bg-active').removeClass("active");
+        $(this).toggleClass("active");               
     });
 });
